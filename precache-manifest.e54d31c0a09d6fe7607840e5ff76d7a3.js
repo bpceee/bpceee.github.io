54732c08708e498c3576c3de8e8bc65f9a907973
bpceee <bipengcheng@gmail.com>
2018-11-21 21:45:17 +0800

    Updates

---
+++ precache-manifest.e54d31c0a09d6fe7607840e5ff76d7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e07271c8d117e24ed26",
+    "url": "/static/js/main.7e07271c.chunk.js"
+  },
+  {
+    "revision": "4f6e85f80a538db8ec59",
+    "url": "/static/js/1.4f6e85f8.chunk.js"
+  },
+  {
+    "revision": "7e07271c8d117e24ed26",
+    "url": "/static/css/main.5a7c88ff.chunk.css"
+  },
+  {
+    "revision": "4f6e85f80a538db8ec59",
+    "url": "/static/css/1.10a7e831.chunk.css"
+  },
+  {
+    "revision": "22864feffccef1c6c3475d75219aacdb",
+    "url": "/index.html"
+  }
+];
--- src/components/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const About = () => (
-  <div>Nothing here!</div>
-)
-
-export default About;
